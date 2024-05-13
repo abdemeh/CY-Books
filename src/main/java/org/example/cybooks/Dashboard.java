80daa8c69ea2a8786c2556d14b3bd6e5ee5b61f5
abdemeh <abdellatif.elmahdaoui@gmail.com>
2024-05-13 03:40:03 +0200

    JFX Commit Login page UI designed

--- src/main/java/org/example/cybooks/Dashboard.java
+++ src/main/java/org/example/cybooks/Dashboard.java
@@ -3,16 +3,19 @@ package org.example.cybooks;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Dashboard extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(CyBooks.class.getResource("dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setTitle("CyBooks - Dashboard");
+        stage.getIcons().add(new Image("file:assets/icon-no-text-white.png"));
         stage.setScene(scene);
         stage.show();
     }
